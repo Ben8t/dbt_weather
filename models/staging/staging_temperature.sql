3e8019d83a99c65dea2e792d4322956057bb0cff
Benoit <benoit@kestra.io>
2024-09-06 14:41:55 +0200

    [fix] proper db/schema names

--- models/staging/staging_temperature.sql
+++ models/staging/staging_temperature.sql
@@ -4,4 +4,4 @@ SELECT
   CAST(latitude AS NUMERIC) AS _latitude,
   CAST(longitude AS NUMERIC) AS _longitude,
   city AS _city
-FROM {{ source('source_api', 'weather') }}
+FROM {{ source('source_api', 'temperature') }}
